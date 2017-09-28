dc1e7fa876e75e1c5fc2b83efc237450b1c7db57
Matt Deuschle <mdeuschle@gmail.com>
2017-09-28 15:24:49 -0700

    UI

--- TDD/ViewController.swift
+++ TDD/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var highScoreLabel: UILabel!
+    @IBOutlet weak var scoreButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonTapped(_ sender: UIButton) {
+
     }
 
+    @IBAction func playAgainTapped(_ sender: UIButton) {
 
+    }
 }
 
